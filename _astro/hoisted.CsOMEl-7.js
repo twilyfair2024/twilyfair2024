3915ba00b4265ec66cabec5161696a2a514a9436
CHooverShrimp <choovershrimp@gmail.com>
2024-08-19 18:21:38 -0700

    schedule

---
+++ _astro/hoisted.CsOMEl-7.js
@@ -0,0 +1,1 @@
+import"./hoisted.DzLBUreC.js";import"./index.CTbVEFV3.js";document.addEventListener("scroll",()=>{window.location.pathname.includes("/main")&&(localStorage.getItem("scroll"),localStorage.setItem("scroll",window.scrollY.toString()))});
